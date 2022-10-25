c805c793acb575c49a780348b699eb1740554cf0
Arshad Ullah <mdarshadullah835@gmail.com>
2022-10-25 10:52:25 +0600

    server created

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const express = require('express');
-const express = app();
 
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
+const course = require('./fackData')
+
+app.get('/', (req, res) => {
+    res.send('learning site  is running ')
+})
+
+app.get('/course', (req, res) => {
+    res.send(course);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = (req.params.id);
+    const course = course.find(ht => ht.id == id);
+    res.send()
+})
+
+app.get('/course')
+app.listen(port, () => {
+    console.log(`Learning site is ${port}`);
+})
